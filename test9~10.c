965255312a45dd1c95663ad687f52115c003f1d8
TaoChenXu <43531241+TaoChenXu@users.noreply.github.com>
2018-10-15 12:12:48 +0800

    Update test9~10.c

--- test9~10.c
+++ test9~10.c
@@ -1,1 +1,21 @@
-
+**9**
+#include<stdio.h>
+int main()
+{
+    int n,k,i,sum;
+ 
+    printf("请输入一个正整数\n");
+    scanf("%d",&n);
+    printf("全部完全数为");
+    for(i=1;i<=n;i++)
+{
+    for(k=1;k<i;k++)
+{
+    if(i%k==0)
+    sum+=k;
+}
+    if(sum==i)
+    printf("%d",i);
+    sum = 0; 
+}
+}
